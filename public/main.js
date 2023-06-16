1771381dbf6ef7e4e3d0cc0c31c14c1b019314a7
Andrew Rudnick <andrewrudnickmi@gmail.com>
2023-06-16 15:54:38 -0400

    option to change voices working for Chrome browser

--- public/main.js
+++ public/main.js
@@ -3,11 +3,42 @@ const generateBtn = document.getElementById('generateBtn')
 const chatlibContainer = document.getElementById('chatlibContainer')
 const submitBtn = document.getElementById('submitBtn')
 const outputContainer = document.getElementById('outputContainer')
+const voiceSelect = document.getElementById('voiceSelect')
 
 generateBtn.addEventListener('click', generateChatLib)
 submitBtn.addEventListener('click', speakChatLib)
 
 let currentChatlib = null
+let voicesLoaded = false
+
+function loadVoices() {
+    const voices = speechSynthesis.getVoices()
+
+    voices.forEach((voice) => {
+        const option = document.createElement('option')
+        option.value = voice.name
+        option.text = `${voice.name} (${voice.lang})`
+        voiceSelect.appendChild(option)
+    })
+
+    voicesLoaded = true
+}
+
+voiceSelect.addEventListener('change', () => {
+    const selectedVoice = voiceSelect.value
+    setVoice(selectedVoice)
+})
+
+function setVoice(voiceName) {
+    const voices = speechSynthesis.getVoices()
+    const selectedVoice = voices.find((voice) => voice.name === voiceName)
+    if (selectedVoice) {
+        speechSynthesis.cancel()
+        speechSynthesis.voice = selectedVoice
+    }
+}
+
+window.speechSynthesis.onvoiceschanged = loadVoices
 
 function generateChatLib() {
     const selectedCategory = categorySelect.value
@@ -31,6 +62,10 @@ function renderChatLib(chatlib) {
 }
 
 function speakChatLib() {
+    // if (!voicesLoaded) {
+    //     alert('Voices are still loading. Please wait a moment and try again.')
+    //     return
+    // }
     const inputs = chatlibContainer.querySelectorAll('input');
     const title = currentChatlib.title
     let finalChatLib = currentChatlib.template;
@@ -40,16 +75,26 @@ function speakChatLib() {
         const placeholder = input.getAttribute('placeholder');
         finalChatLib = finalChatLib.replace(`[${placeholder}]`, value);
     });
+
+    const selectedVoice = voiceSelect.value
+    const voices = speechSynthesis.getVoices()
+    const voice = voices.find((v) => v.name === selectedVoice)
   
-    const utterance = new SpeechSynthesisUtterance();
-    utterance.text = `${title}. ${finalChatLib}`
+    if (voice) {
+        const utterance = new SpeechSynthesisUtterance();
+        utterance.text = `${title}. ${finalChatLib}`
+        utterance.rate = 0.8
+        utterance.voice = voice
 
-    utterance.addEventListener('start', () => {
-        setTimeout(() => {
-            speechSynthesis.resume()
-        }, 500)
-    })
+        utterance.addEventListener('start', () => {
+            setTimeout(() => {
+                speechSynthesis.resume()
+            }, 500)
+        })
 
-    speechSynthesis.speak(utterance);
-    outputContainer.textContent = finalChatLib;
+        speechSynthesis.speak(utterance);
+        outputContainer.textContent = finalChatLib;
+    } else {
+        alert('Selected voice is not available.')
+    }
 }
