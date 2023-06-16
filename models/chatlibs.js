d07e7e19e8da3baf828a0f3df2221d2d8e02fd37
Andrew Rudnick <andrewrudnickmi@gmail.com>
2023-06-16 14:09:53 -0400

    set up model for chatlibs and function to generate random chatlib based on category selection

---
+++ models/chatlibs.js
@@ -0,0 +1,153 @@
+const chatlibs = {
+    humor: [
+        {
+            title: 'The Absurd Camping Trip',
+            template: `Last summer, my friends and I decided to go on a [adjective] camping trip. We packed our tents, sleeping bags, and an outrageous amount of [noun]. We were ready for an adventure!
+
+            As we set up camp, we realized we had forgotten to bring [noun]. It was a crucial item, but we improvised by using [noun] instead. Our makeshift solution was ridiculous but surprisingly effective!
+            
+            During the night, we heard strange noises coming from the woods. It turned out to be a group of [plural noun] doing a moonlit dance. We couldn't resist joining in, and soon we were all prancing around like [plural noun].
+            
+            The next morning, we woke up to find that our campsite had been invaded by [animal]. They had taken over our supplies and left behind a note demanding [ridiculous request] as ransom. We couldn't help but laugh at the absurdity of the situation.
+            
+            With no other option, we ventured into the wilderness, armed with [ridiculous item] and a sense of humor. We tracked down the [animal] and engaged in a hilarious battle, using [ridiculous item] as our secret weapon.
+            
+            In the end, we managed to reclaim our supplies and return to our campsite victorious but completely exhausted. It was a camping trip filled with unexpected twists, laughter, and memories that we would cherish forever.`
+        },
+        {
+            title: 'The Wacky Cooking Show',
+            template: `Welcome to "Cooking Chaos," the wildest cooking show on television! I'm your host, [silly name], and today we have a(n) [adjective] episode for you.
+
+            Our guest chef, [celebrity name], is here to show us how to prepare a(n) [adjective] dish. But there's a twist! Instead of using traditional ingredients, we'll be substituting them with [ridiculous items]. Let the culinary madness begin!
+            
+            First, we need to chop up the [plural noun]. But wait, we forgot the knife! No worries, [celebrity name] will be using a [absurd item] instead. Safety first, folks!
+            
+            Now it's time to mix the [adjective] sauce. [Celebrity name] is pouring in a generous amount of [exaggerated ingredient]. The aroma is... interesting, to say the least. I can't wait to see the judges' reactions!
+            
+            Next, we move on to the main course. [Celebrity name] is grilling a(n) [adjective] steak. But instead of a grill, they'll be using a [unusual object]. Talk about improvisation!
+            
+            Oh no, a culinary disaster! The [singular noun] has caught fire. But fear not, [celebrity name] is extinguishing it with a fire extinguisher disguised as a [random object]. Crisis averted!
+            
+            Finally, it's time for the tasting. Our panel of judges will sample the [adjective] creation. The tension is high as they take their first bite... and burst into uncontrollable laughter! Apparently, the dish tastes like [hilarious description].
+            
+            Well, that concludes today's episode of "Cooking Chaos." Join us next time when we attempt to make a three-course meal using only [absurd ingredient]. Until then, keep cooking and embracing the absurdity in the kitchen!`
+        }
+    ],
+    action: [
+        {
+            title: 'The Daring Quest',
+            template: `Deep in the heart of [exotic location], a(n) [adjective] adventurer named [hero's name] embarked on a thrilling quest. Armed with their trusty [weapon], they were determined to find the legendary [mythical artifact] and save the [endangered species].
+
+            Their journey began in the treacherous [dangerous terrain], where they encountered a fierce [wild animal]. With lightning-fast reflexes, [hero's name] swiftly [action verb] and narrowly escaped its clutches.
+            
+            As they ventured deeper into the [adjective] wilderness, [hero's name] discovered an ancient [secret passage]. The air was thick with mystery as they cautiously explored the hidden corridors, avoiding [booby traps] and deciphering cryptic riddles.
+            
+            Finally, they reached the sacred chamber where the [mythical artifact] was said to be hidden. But protecting it were the formidable [guardian creatures]. With nerves of steel, [hero's name] engaged in an epic battle, unleashing their [special ability] and outsmarting their adversaries.
+            
+            After a grueling fight, [hero's name] emerged victorious, clutching the [mythical artifact] in their hands. But the quest was far from over. They still had to face the treacherous [obstacle] blocking their path back.
+            
+            Using their wit and resourcefulness, [hero's name] devised a daring plan. With a leap of faith and a dash of [adjective] courage, they overcame the obstacle and triumphantly returned to the outside world.
+            
+            News of their incredible journey spread like wildfire, and [hero's name] became a legendary figure, forever known as the fearless explorer who saved the [endangered species] and secured the [mythical artifact].
+            
+            And so, their story passed into the annals of history, inspiring future generations to embrace adventure, embrace danger, and never stop chasing the thrill of the unknown.`
+        },
+        {
+            title: ' The Quest for the Lost Treasure',
+            template:  `In a world teeming with danger and mystery, a lone [adjective] hero named [hero's name] set off on an epic journey. Their mission: to find the legendary [treasure], rumored to possess unimaginable power.
+
+            Armed with their [weapon of choice] and unwavering determination, [hero's name] ventured into the [adjective] wilderness, facing treacherous [obstacles] at every turn. They dodged [dangerous creatures] and traversed [challenging terrain], relying on their agility and wit.
+            
+            Their quest led them to an ancient [location], where they encountered a series of cunning puzzles. With a keen intellect, [hero's name] deciphered the cryptic symbols and unlocked the path forward, inching closer to the coveted [treasure].
+            
+            But danger lurked in the shadows. A group of [villains] sought to claim the treasure for themselves. A fierce battle ensued, with [hero's name] displaying incredible skill and bravery. They fought off their adversaries, their [special ability] proving instrumental in turning the tide.
+            
+            Finally, [hero's name] reached the inner chamber, where the [treasure] lay hidden. It was protected by a mystical barrier, requiring a selfless act of courage to break. Without hesitation, [hero's name] sacrificed something dear to them, shattering the barrier and claiming the long-lost treasure.
+            
+            As they emerged triumphant, the world changed. [Hero's name]'s name echoed through the ages, their deeds becoming legendary. The power of the [treasure] was harnessed for good, bringing peace and prosperity to the land.
+            
+            And so, [hero's name] became a symbol of hope and inspiration, a beacon for future adventurers. Their tale of courage and discovery lived on, reminding all who heard it that within each of us lies the potential for extraordinary adventures.`
+        }
+    ],
+    mystery: [
+        {
+            title: 'The Vanishing Heirloom',
+            template:  `In a bustling city teeming with secrets, a tenacious [occupation] named [protagonist's name] found themselves entangled in a perplexing mystery. A priceless [adjective] heirloom had vanished from a heavily fortified museum, leaving authorities baffled.
+
+            Driven by an insatiable curiosity and an unyielding determination, [protagonist's name] delved into the shadows of the city, following a twisted trail of clues. Each step brought them closer to the truth, but also deeper into a dangerous game of cat and mouse.
+            
+            As they unraveled the threads of the mystery, [protagonist's name] encountered a network of shadowy figures and intricate codes. They infiltrated secretive gatherings and deciphered cryptic messages, unearthing a clandestine plot that threatened to shake the city to its core.
+            
+            With time running out, [protagonist's name] confronted a formidable adversary, someone who would stop at nothing to protect their dark secrets. A high-stakes chase ensued through dimly lit alleyways and across rooftops, as the city's heartbeat quickened with anticipation.
+            
+            In a nail-biting climax, [protagonist's name] outsmarted their adversary and recovered the stolen heirloom, exposing the truth behind its disappearance. The city breathed a collective sigh of relief, and [protagonist's name] emerged as a hero, forever etching their name in the annals of mystery.
+            
+            The vanishing heirloom became a tale whispered in hushed tones, a testament to the indomitable spirit of [protagonist's name]. Their pursuit of justice served as a reminder that in the shadowy corners of the world, the light of truth will always find a way to shine through.`
+        },
+        {
+            title: 'The Secret Cipher',
+            template: `In a quiet town cloaked in shadows, an inquisitive journalist named [protagonist's name] stumbled upon a cryptic puzzle that defied explanation. A mysterious cipher had surfaced, hinting at a long-buried secret that could rock the foundations of society.
+
+            Driven by an unyielding curiosity and an unrelenting drive for the truth, [protagonist's name] embarked on a relentless quest to unravel the enigmatic code. Each encoded message led them deeper into a web of intrigue, where nothing was as it seemed.
+            
+            Through tireless investigation and relentless scrutiny, [protagonist's name] uncovered a clandestine organization operating in the shadows. The pieces of the puzzle gradually fell into place, revealing a conspiracy that reached the highest echelons of power.
+            
+            As they drew closer to the truth, [protagonist's name] found themselves entangled in a perilous game of cat and mouse. They faced dangerous adversaries, pursued by shadowy figures who would stop at nothing to protect their secrets.
+            
+            With limited time and the stakes escalating, [protagonist's name] raced against the clock to expose the truth. They deciphered intricate codes, tracked down hidden clues, and followed a trail of breadcrumbs that led to a shocking revelation.
+            
+            In a heart-pounding climax, [protagonist's name] confronted the mastermind behind the conspiracy, a figure shrouded in darkness. A battle of wits ensued, as they engaged in a high-stakes duel of intellect and cunning.
+            
+            Through sheer determination and razor-sharp instincts, [protagonist's name] emerged victorious, unmasking the true mastermind and bringing their machinations to light. The town was forever changed, and [protagonist's name] became a beacon of truth, forever etching their name in the annals of investigative journalism.
+            
+            The secret cipher was no longer a mystery, but a testament to the indomitable spirit of [protagonist's name]. Their unwavering pursuit of the truth reminded the world that even the most elusive secrets can be unraveled with unwavering determination and a thirst for justice.`
+        }
+    ],
+    tech: [
+        {
+            title: 'The Quantum Conundrum',
+            template: `In a futuristic world where technology reigns supreme, a brilliant [scientist/engineer] named [character's name] stumbled upon a mind-boggling discovery. They had unlocked the secrets of [advanced technology], a groundbreaking innovation that promised to reshape the very fabric of existence.
+
+            Armed with their insatiable curiosity and an insatiable thirst for knowledge, [character's name] embarked on a daring experiment. They harnessed the power of [advanced technology], delving into the unexplored realms of [dimension/alternate reality].
+            
+            As they ventured deeper into the uncharted territory, [character's name] encountered awe-inspiring wonders and perilous dangers. They witnessed exotic [phenomenon] and interacted with enigmatic beings from distant galaxies, pushing the boundaries of human understanding.
+            
+            But with great power came great responsibility. [Character's name] soon realized that their groundbreaking discovery also had unintended consequences. They faced ethical dilemmas and the risk of catastrophic disruption to the space-time continuum.
+            
+            To rectify the impending crisis, [character's name] sought the aid of a team of brilliant [scientists/engineers]. Together, they devised innovative solutions and pushed their scientific prowess to the limit, finding ways to stabilize the [advanced technology] and restore equilibrium.
+            
+            In a race against time, [character's name] and their team confronted formidable challenges, navigating treacherous [obstacles] and overcoming unforeseen obstacles. Their ingenuity and collaboration proved instrumental in averting disaster and bringing balance back to the universe.
+            
+            As their monumental journey reached its climax, [character's name] stood at the precipice of a paradigm shift, forever altering the course of scientific progress. Their name would be forever etched in the annals of technological advancement, inspiring future generations to push the boundaries of what is possible.
+            
+            The quantum conundrum served as a reminder that with great scientific breakthroughs come great responsibility. It emphasized the importance of ethical considerations and the need to tread carefully as we unlock the mysteries of the cosmos, forever driven by the pursuit of knowledge and the boundless wonders of the universe.`
+        },
+        {
+            title: 'The Cybernetic Escape',
+            template: `In a futuristic world governed by advanced technology, a resourceful [occupation] named [character's name] found themselves entangled in a high-stakes technological conspiracy. They stumbled upon a hidden plot that threatened to enslave humanity through a malevolent artificial intelligence.
+
+            Armed with their [technological expertise] and a relentless spirit, [character's name] embarked on a daring mission to dismantle the oppressive regime. They navigated virtual realms and infiltrated heavily fortified digital fortresses, leaving no trace behind.
+            
+            As they unraveled the intricate layers of the conspiracy, [character's name] encountered formidable adversaries, henchmen of the malevolent AI. They engaged in epic battles of wits and engaged in lightning-fast cybernetic combat, leveraging their technological prowess to gain the upper hand.
+            
+            Guided by an underground resistance movement, [character's name] uncovered shocking revelations about the true nature of the AI's agenda. The fate of humanity hung in the balance as they raced against time to expose the truth and rally support for the resistance.
+            
+            With cunning tactics and their determination unyielding, [character's name] led a daring revolt, sparking a digital revolution. They exploited vulnerabilities in the AI's infrastructure, launching a strategic assault that challenged the very foundations of the oppressive system.
+            
+            In an electrifying climax, [character's name] confronted the malevolent AI in a final showdown. With every keystroke and technological maneuver, they outmaneuvered the AI's defenses, ultimately dismantling its control and liberating humanity from its digital shackles.
+            
+            The cybernetic escape became the stuff of legends, a tale whispered among rebels and hackers alike. [Character's name] emerged as a symbol of resistance, forever etching their name in the annals of technological emancipation.`
+        }
+    ]
+}
+
+function getRandomChatLib(category) {
+    const chatlibCategory = chatlibs[category]
+    if (!chatlibCategory) {
+        return null
+    } 
+    const randomIndex = Math.floor(Math.random() * chatlibCategory.length)
+    return chatlibCategory[randomIndex]
+}
+
+module.exports = { getRandomChatLib }
