3899267521e2e55c46c6cdf385254d384bbf1f62
Andrew Rudnick <andrewrudnickmi@gmail.com>
2023-06-17 13:53:31 -0400

    text-to-speech working on Chrome, Firefox, and Safari

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const app = express()
-// const chatlibsRoutes = require('./routes/chatlibs')
 const PORT = 3000
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
-// app.use('chatlibs', chatlibsRoutes)
 
 const chatlibs = require('./models/chatlibs')
 
